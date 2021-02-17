73ac297c3e8d965dfa1c6962e28b74b92ffcd65d
Ketan <pathareketan1@gmail.com>
2021-02-17 14:18:45 +0530

    ok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfileScreen from "./components/ProfileScreen";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <ProfileScreen />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./css/_styles.scss";
+
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
+);
